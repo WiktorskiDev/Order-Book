a9cbd30680d55e3ce8452cffc5e1a659a273ced6
WiktorskiDev <cezary@wiktorski.dev>
2026-07-06 16:08:08 +0100

    Added market and limit order structs

--- include/core_data_structs.h
+++ include/core_data_structs.h
@@ -2,22 +2,38 @@
 
 #include <cstdint>
 
-enum class orderSide{
+enum class OrderSide{
     Bid,
     Ask
 };
 
-struct order{
+struct limitOrder{
     std::uint64_t orderID;
-    std::uint64_t priceLevel;
+    std::uint64_t price;
     std::uint64_t quantity;
+    std::uint64_t timestampNs;
+    OrderSide side;
+
+    limitOrder(std::uint64_t id, std::uint64_t prc, std::uint64_t qty, std::uint64_t timeNs, OrderSide sd)
+        :orderID(id),
+        price(prc),
+        quantity(qty),
+        timestampNs(timeNs),
+        side(sd)
+        {};
+};
+
+struct marketOrder{
     
-};
+    std::uint64_t orderID;
+    std::uint64_t quantity;
+    std::uint64_t timestampNs;
+    OrderSide side;
 
-struct limitOrder : order{
-
-};
-
-struct marketOrder : order{
-
+    marketOrder(std::uint64_t id, std::uint64_t qty, std::uint64_t timeNs, OrderSide sd)
+        :orderID(id),
+        quantity(qty),
+        timestampNs(timeNs),
+        side(sd)
+        {};
 };
